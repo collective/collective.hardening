977ce01a3788c01c01fa233aee4dab7b4e11f910
ale-rt <alessandro.pisa@gmail.com>
2025-01-10 11:51:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+[![image](https://github.com/collective/collective.hardening/actions/workflows/plone-package.yml/badge.svg)](https://github.com/collective/collective.hardening/actions/workflows/plone-package.yml)
+[![Coveralls](https://coveralls.io/repos/github/collective/collective.hardening/badge.svg?branch=main)](https://coveralls.io/github/collective/collective.hardening?branch=main)
+[![image](https://codecov.io/gh/collective/collective.hardening/branch/master/graph/badge.svg)](https://codecov.io/gh/collective/collective.hardening)
+[![Latest Version](https://img.shields.io/pypi/v/collective.hardening.svg)](https://pypi.python.org/pypi/collective.hardening/)
+[![Egg Status](https://img.shields.io/pypi/status/collective.hardening.svg)](https://pypi.python.org/pypi/collective.hardening)
+![image](https://img.shields.io/pypi/pyversions/collective.hardening.svg?style=plastic%20%20%20:alt:%20Supported%20-%20Python%20Versions)
+[![License](https://img.shields.io/pypi/l/collective.hardening.svg)](https://pypi.python.org/pypi/collective.hardening/)
+
+# collective.hardening
+
+An addon for hardening Plone.
+
+## Installation
+
+Install `collective.hardening` by adding it to your buildout, e.g.:
+
+    [instance]
+    eggs +=
+        collective.hardening
+
+and then running `bin/buildout`.
+
+When your instance starts you can enter a pdb session by adding the path `pdb` to any URL, e.g.:
+
+- http://localhost:8080/pdb
+- http://localhost:8080/Plone/pdb
+
+
+## Authors
+
+The [Syslab.com](https://www.syslab.com) Team.
+
+## Contributors
+
+Put your name here, you deserve it!
+
+- Alessandro Pisa, [Syslab.com](https://www.syslab.com)
+
+## Contribute
+
+- Issue Tracker:
+  <https://github.com/collective/collective.hardening/issues>
+- Source Code: <https://github.com/collective/collective.hardening>
+- Documentation: <https://docs.plone.org/foo/bar>
+
+## Support
+
+If you are having issues, please let us know in the [issue tracker](https://github.com/collective/collective.hardening/issues).
+
+## License
+
+The project is licensed under the GPLv2.
---
+++ setup.py
@@ -0,0 +1,65 @@
+"""Installer for the collective.hardening package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = "\n\n".join(
+    [
+        open("README.md").read(),
+        open("CONTRIBUTORS.md").read(),
+        open("CHANGES.md").read(),
+    ]
+)
+
+
+setup(
+    name="collective.hardening",
+    version="1.0.0a1.dev0",
+    description="An addon for hardening Plone",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # Get more from https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords="Python Plone CMS Hardening",
+    author="ale-rt",
+    author_email="alessandro.pisa@gmail.com",
+    url="https://github.com/collective/collective.hardening",
+    project_urls={
+        "PyPI": "https://pypi.org/project/collective.hardening/",
+        "Source": "https://github.com/collective/collective.hardening",
+        "Tracker": "https://github.com/collective/collective.hardening/issues",
+    },
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["collective"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.9",
+    install_requires=["setuptools", "Products.CMFPlone"],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
