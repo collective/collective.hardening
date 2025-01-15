eee25b4cd83bbbb9141fac41e1962f742ed9db3e
ale-rt <alessandro.pisa@gmail.com>
2025-01-15 09:16:22 +0100

    Back to development: 0.0.3

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.0.3 (unreleased)
+
+
+- Nothing changed yet.
+
+
 ## 0.0.2 (2025-01-15)
 
 
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.hardening",
-    version="0.0.2",
+    version="0.0.3.dev0",
     description="An addon for hardening Plone",
     long_description=long_description,
     long_description_content_type="text/markdown",
